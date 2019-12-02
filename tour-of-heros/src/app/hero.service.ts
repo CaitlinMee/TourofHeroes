31d48b6171d46d514a3f6837794a378b4a3cd8fc
CaitlinMee <57130231+CaitlinMee@users.noreply.github.com>
2019-12-02 20:01:45 +1100

    Update getHero to be by ID

--- tour-of-heros/src/app/hero.service.ts
+++ tour-of-heros/src/app/hero.service.ts
@@ -42,11 +42,13 @@ export class HeroService {
         return of(result as T);
     };
   }
-
+/** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-     // TODO: send the message _after_ fetching the hero
-     this.messageService.add(`HeroService: fetched hero id=${id}`);
-     return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /** PUT: update the hero on the server */
